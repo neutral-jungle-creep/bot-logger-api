4f342fc11ff3e6faa1ab284144a9b59fd2f6fa9f
neutral-jungle-creep <arrrseny11@gmail.com>
2022-10-23 16:15:25 +0300

    add config init.go

--- cmd/services-front.go
+++ cmd/services-front.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
+	"services-front/config"
 	"services-front/pkg"
 	"services-front/pkg/handler"
 	"services-front/pkg/service"
@@ -9,12 +11,16 @@ import (
 )
 
 func main() {
+	if err := config.InitConfig(); err != nil {
+		log.Fatalf("init config error: %s", err.Error())
+	}
+
 	storages := storage.NewStorage()
 	services := service.NewService(storages)
 	handlers := handler.NewHandler(services)
 
 	srv := new(pkg.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error http server, %s", err.Error())
 	}
 }
