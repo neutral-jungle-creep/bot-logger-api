9e840393c6408028cfed18302fff5de4f04dd399
neutral-jungle-creep <arrrseny11@gmail.com>
2022-10-30 16:36:59 +0300

    add test show func & add db conn to main

--- cmd/services-front.go
+++ cmd/services-front.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"services-front/configs"
 	"services-front/pkg"
 	"services-front/pkg/handler"
+	"services-front/pkg/service"
+	"services-front/pkg/storage"
 )
 
 func main() {
@@ -13,7 +16,15 @@ func main() {
 		logrus.Fatalf("init configs error: %s", err.Error())
 	}
 
-	handlers := handler.NewHandler()
+	db, err := storage.NewConnect(viper.GetString("dbLink"))
+	if err != nil {
+		logrus.Fatalf("connect db error: %s", err.Error())
+	}
+	defer db.Close(context.Background())
+
+	stor := storage.NewStorage(db)
+	serv := service.NewService(stor)
+	handlers := handler.NewHandler(serv)
 
 	srv := new(pkg.Server)
 	if err := srv.Run(viper.GetString("httpPort"), handlers.InitRoutes()); err != nil {
--- pkg/handler/show.go
+++ pkg/handler/show.go
@@ -1,7 +1,13 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func (h *Handler) messages(c *gin.Context) {
-
+	userId, _ := c.Get(userCtx)
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"ответ": userId,
+	})
 }
