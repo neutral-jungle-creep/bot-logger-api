2450370727b8f134149a1b6489aa57acbd380064
neutral-jungle-creep <arrrseny11@gmail.com>
2022-10-31 19:30:17 +0300

    move queries to config

--- pkg/storage/auth.go
+++ pkg/storage/auth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"github.com/jackc/pgx/v4"
+	"github.com/spf13/viper"
 	"services-front/pkg/domain"
 )
 
@@ -18,8 +19,12 @@ func NewAuthStorage(conn *pgx.Conn) *AuthStorage {
 }
 
 func (s *AuthStorage) CreateUser(user *domain.User) error {
-	query := `UPDATE public.users SET user_name=$1, user_password=$2 WHERE tg_user_id=$3`
-	_, err := s.conn.Exec(context.Background(), query, user.Username, user.Password, user.Id)
+	_, err := s.conn.Exec(context.Background(), viper.GetString("queries.createUser"),
+		user.Username,
+		user.Password,
+		user.Id,
+	)
+
 	if err != nil {
 		return err
 	}
@@ -28,9 +33,11 @@ func (s *AuthStorage) CreateUser(user *domain.User) error {
 
 func (s *AuthStorage) GetTgChatMember(user *domain.User) error {
 	var userId int
-	query := `SELECT id FROM public.users WHERE tg_user_id=$1 AND active_user=TRUE`
 
-	result := s.conn.QueryRow(context.Background(), query, user.Id)
+	result := s.conn.QueryRow(context.Background(), viper.GetString("queries.getTgChatMember"),
+		user.Id,
+	)
+
 	if err := result.Scan(&userId); err != nil {
 		return errors.New("пользователь не найден в чате телеграм, для того, чтобы зарегистрироваться " +
 			"или авторизоваться получите приглашение в чат")
@@ -40,9 +47,13 @@ func (s *AuthStorage) GetTgChatMember(user *domain.User) error {
 
 func (s *AuthStorage) GetUser(user *domain.User) (int, error) {
 	var userId int
-	query := `SELECT id FROM public.users WHERE tg_user_id=$1 AND user_name=$2 AND user_password=$3`
 
-	result := s.conn.QueryRow(context.Background(), query, user.Id, user.Username, user.Password)
+	result := s.conn.QueryRow(context.Background(), viper.GetString("queries.getUser"),
+		user.Id,
+		user.Username,
+		user.Password,
+	)
+
 	if err := result.Scan(&userId); err != nil {
 		return 0, errors.New("аккаунт не найден, проверьте правильность введенных данных")
 	}
