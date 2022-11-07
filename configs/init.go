aaf795a2d14cf07201520171eb79fe50a90f1d70
neutral-jungle-creep <arrrseny11@gmail.com>
2022-11-07 21:01:15 +0300

    add config_template.yml

--- configs/init.go
+++ configs/init.go
@@ -4,6 +4,6 @@ import "github.com/spf13/viper"
 
 func InitConfig(path string) error {
 	viper.AddConfigPath(path)
-	viper.SetConfigName("config")
+	viper.SetConfigName("config_template")
 	return viper.ReadInConfig()
 }
